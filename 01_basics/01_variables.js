05cf5cc5426967db12f568ed0a72dcf34c56564d
Ashutosh Sahu <ashutosh.sahu2015@gmail.com>
2024-07-04 05:01:53 +0000

    adding basics and datatypes code along with configuration changes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "ashutosh@gmail.com"
+var accountPassword = "12345"
+accountCity = "Bangalore"
+let accountState
+
+accountEmail = "ashu@gmail.com"
+accountPassword = "212121"
+accountCity = "Nagpur"
+
+console.log(accountId);
+
+// don't use var for declearning variables because it has the scope issues
+
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all the JS code as newer version
+
+//alert(3+3) // we are using nodejs ,not browser
+
+console.log(3
+    +
+    3
+) // this is wrong way of writing, code readability should be high
+
+console.log("Ashutosh")
+
+// Datatypes
+let name = "Ashutosh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+//number ->2^53
+//bigint
+//string -> ""
+//boolean -> true/false
+//nulll -> standalone value
+//undefined -> 
+//symbol -> unique
+
+//object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object

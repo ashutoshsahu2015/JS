5ae5b5ccd7bb63e7c4f6fa2d6023e3aeaf74179d
Ashutosh Sahu <ashutosh.sahu2015@gmail.com>
2024-08-01 20:58:04 +0530

    Added the object const code

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -8,6 +8,13 @@ accountEmail = "ashu@gmail.com"
 accountPassword = "212121"
 accountCity = "Nagpur"
 
+// object that are const, the object properties can be updated, added or removed
+
+const obj1 = {key1: 2}
+obj1.key1 = 5
+
+console.log(obj1)
+
 console.log(accountId);
 
 // don't use var for declearning variables because it has the scope issues

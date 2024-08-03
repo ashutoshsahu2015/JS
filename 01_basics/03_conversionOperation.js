461b7911b79e04d86c6933712a952ef6d32a8afc
Ashutosh Sahu <ashutosh.sahu2015@gmail.com>
2024-08-03 16:48:46 +0530

    writing output for conversion operator and data type changes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -10,6 +10,8 @@ console.log(valueInNumber);
 
 // "33" -> 33
 // "33abc" -> NaN
+// null -> 0
+// undefined -> NaN
 // true -> 1; false -> 0
 
 let isLoggedIn = "Ashutosh"
@@ -23,8 +25,8 @@ console.log(booleanIsLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+console.log(stringNumber); // 33
+console.log(typeof stringNumber); // string
 
 // ************************************ Operations ********************************
 
@@ -45,17 +47,17 @@ let str2 = " Ashutosh"
 let str3 = str1 + str2
 console.log(str3);
 
-console.log("1"+2);
-console.log(1 + "2");
-console.log("1"+2+2);
-console.log(1+2+"2");
+console.log("1"+2); // 12
+console.log(1 + "2"); // 12
+console.log("1"+2+2); // 122
+console.log(1+2+"2"); // 32
 
-console.log(+true);
-console.log(+"");
+console.log(+true); // 1
+console.log(+""); // 0
 
 let num1,num2,num3
 
-num1 = num2 = num3 = 2+2
+num1 = num2 = num3 = 2+2 // bad practice of writing code
 
 let gameCounter = 100
 ++gameCounter;

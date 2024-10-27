705f0c55ab4302e3a76448844ee92266ff15f086
Ashutosh Sahu <ashutosh.sahu2015@gmail.com>
2024-10-27 04:02:44 +0000

    control flow

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,32 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "feb"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+
+    case "feb":
+        console.log("February");
+        break;
+    
+    case "march":
+        console.log("March");
+        break;
+    
+    case "april":
+        console.log("April");
+        break;
+
+    default:
+        console.log("Defualt Case Match")
+        break;
+}
